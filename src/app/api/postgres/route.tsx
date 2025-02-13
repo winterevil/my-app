d4fd2568f2ff0c8828881ac8598a26ede3f1f770
winterevil <long.lethanh.cit21@eiu.edu.vn>
2025-02-13 13:06:18 +0700

    refactor: deleting unused code

--- src/app/api/postgres/route.tsx
+++ src/app/api/postgres/route.tsx
@@ -1,5 +1,4 @@
 import { NextResponse } from 'next/server';
-// import nodemailer from 'nodemailer';
 import { query } from '@/lib/db';
 
 export async function POST(req: Request) {
@@ -14,23 +13,6 @@ export async function POST(req: Request) {
             'INSERT INTO messages (email, subject, messages) VALUES ($1, $2, $3) RETURNING *',
             [email, subject, message]
         );
-
-        // const transporter = nodemailer.createTransport({
-        //     service: 'gmail',
-        //     auth: {
-        //         user: process.env.EMAIL_USER,
-        //         pass: process.env.EMAIL_PASS,
-        //     },
-        // });
-
-        // const mailOptions = {
-        //     from: process.env.EMAIL_USER,
-        //     to: email,
-        //     subject: `Re: ${subject}`,
-        //     text: `Thank you for reaching out!\n\nWe have received your message:\n"${message}"\n\nWe will get back to you as soon as possible.\n\nBest regards!`,
-        // };
-
-        // await transporter.sendMail(mailOptions);
 
         return NextResponse.json({ message: 'Message sent successfully', data: result.rows[0] }, { status: 200 });
     } catch (error) {
