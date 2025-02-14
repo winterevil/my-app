71b0a00efa01ca23e72e57d6617737138b8a7139
winterevil <long.lethanh.cit21@eiu.edu.vn>
2025-02-14 11:31:39 +0700

    fix: spam detected

--- src/app/api/postgres/route.tsx
+++ src/app/api/postgres/route.tsx
@@ -1,28 +1,18 @@
 import { NextResponse } from 'next/server';
 import { query } from '@/lib/db';
-// import nodemailer from 'nodemailer';
-
-// const emailHistory = new Map(); 
 
 export async function POST(req: Request) {
     try {
-        const { email, subject, message } = await req.json();
+        const { email, subject, message, honeypot } = await req.json();
 
-        if (!email || !subject || !message) {
-            return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+        if (honeypot) {
+            return NextResponse.json({ error: 'Spam detected!' }, { status: 400 });
         }
 
-        // if (message.includes("http://") || message.includes("https://")) {
-        //     return NextResponse.json({ error: "Spam detected!" }, { status: 400 });
-        // }
-
-        // const now = Date.now();
-
-        // if (emailHistory.has(email) && now - emailHistory.get(email) < 120000) {
-        //     return NextResponse.json({ error: "Too many requests, try again later" }, { status: 429 });
-        // }
-
-        // emailHistory.set(email, now);
+        const containsURL = (text: string) => /(https?:\/\/[^\s]+)/.test(text);
+        if (containsURL(message)) {
+            return NextResponse.json({ error: "Spam detected!" }, { status: 400 });
+        }
 
         const result = await query(
             'INSERT INTO messages (email, subject, messages) VALUES ($1, $2, $3) RETURNING *',
@@ -57,3 +47,6 @@ export async function POST(req: Request) {
         return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
     }
 }
+
+
+
